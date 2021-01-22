d95deb6b8fb8ebdb9e066cfe2f3a693700562e77
RomainGuarinoni <romain.guar91@gmail.com>
2021-01-22 22:59:52 +0100

    creation de l'infrastructure des données et du menu de sélection du niveau

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,165 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="menu" v-if="menu">
+      <h1>Améliore ton rythme</h1>
+      <h2>Choisis un niveau de difficulté</h2>
+      <div class="levelBox">
+        <div
+          class="level"
+          v-for="(item, index) in ['niveau 1 ', 'niveau 2 ', 'niveau 3']"
+          :key="item"
+          @click="goToLevel(index)"
+        >
+          <p>{{ item }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="game" v-else></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    let noteGlobal = [
+      {
+        img: require("@/assets/ronde.svg"),
+        value: 1,
+      },
+      {
+        img: require("@/assets/blanche.svg"),
+        value: 2,
+      },
+      {
+        img: require("@/assets/noir.svg"),
+        value: 3,
+      },
+      {
+        img: require("@/assets/noirpoint.svg"),
+        value: 4,
+      },
+      {
+        img: require("@/assets/croche.svg"),
+        value: 5,
+      },
+      {
+        img: require("@/assets/pause.svg"),
+        value: 6,
+      },
+      {
+        img: require("@/assets/demiPause.svg"),
+        value: 7,
+      },
+      {
+        img: require("@/assets/soupir.svg"),
+        value: 8,
+      },
+      {
+        img: require("@/assets/demiSoupirPoint.svg"),
+        value: 9,
+      },
+      {
+        img: require("@/assets/demiSoupir.svg"),
+        value: 10,
+      },
+    ];
+    return {
+      menu: true,
+      prop: Object,
+      diff: [
+        [
+          {
+            mp3: require("@/assets/level1_1.mp3"),
+            result: [2, 2],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+          {
+            mp3: require("@/assets/level1_2.mp3"),
+            result: [3, 3, 2],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+          {
+            mp3: require("@/assets/level1_3.mp3"),
+            result: [3, 3, 3, 3],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+          {
+            mp3: require("@/assets/level1_4.mp3"),
+            result: [2, 3, 3],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+          {
+            mp3: require("@/assets/level1_5.mp3"),
+            result: [3, 2, 3],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+          {
+            mp3: require("@/assets/level1_6.mp3"),
+            result: [3, 3, 2],
+            note: [noteGlobal[1], noteGlobal[2], noteGlobal[3], noteGlobal[4]],
+          },
+        ],
+        [],
+        [],
+      ],
+    };
+  },
+  methods: {
+    goToLevel(level) {
+      let random = Math.floor(Math.random() * this.diff[level].length);
+      this.prop = this.diff[level][random];
+      console.log(this.prop);
+      this.menu = false;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --main: #424242;
+}
+h1,
+h2 {
+  color: var(--main);
+  letter-spacing: 2px;
+}
+.menu {
+  font-family: "Crete Round", serif;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 0;
+}
+.levelBox {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  flex: 1;
+  justify-content: space-around;
+  align-items: center;
+}
+.level {
+  width: 50%;
+  border: 2px solid grey;
+  border-radius: 20px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  cursor: pointer;
+  transition: all ease 200ms;
+  color: var(--main);
+}
+.level:hover {
+  box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.521);
+  transform: scale(1.02);
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,176 @@
+<template>
+  <div id="app">
+    <h1>Améliore ton rythme</h1>
+    <div class="gameBox">
+      <div class="header">
+        <div class="left">
+          <p>Clique ici pour écouter l'audio :</p>
+          <div class="playButton" @click="playAudio">
+            <font-awesome-icon v-if="play" :icon="['fas', 'play']" />
+            <p v-else style="font-size:15px">En lecture</p>
+          </div>
+        </div>
+        <div class="right">
+          <p>
+            Nombre restant d'écoute :
+            <span class="nbEcoute" :style="{ color: color[3 - nbEcoute] }">{{
+              nbEcoute
+            }}</span>
+          </p>
+        </div>
+      </div>
+      <!--<div class="metronome">
+        <p>4</p>
+        <p>3</p>
+        <p>2</p>
+        <p>1</p>
+      </div>-->
+      <div class="boxButton">
+        <Button
+          @CHECK="check"
+          v-for="item in test[0].note"
+          :key="item.index"
+          :index="item.index"
+          :indexEnCours="indexLive"
+          :note="item.img"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button.vue";
+export default {
+  name: "App",
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      indexLive: 0,
+      nbEcoute: 3,
+      play: true,
+      color: ["#43A047", "#43A047", "#FBC02D", "#D50000"],
+      test: [
+        {
+          audio: require("./assets/sound1.mp3"),
+          bpm: 120,
+          note: [
+            {
+              img: require("./assets/noir.svg"),
+              index: 0,
+            },
+            {
+              img: require("./assets/croche.svg"),
+              index: 1,
+            },
+            {
+              img: require("./assets/blanche.svg"),
+              index: 2,
+            },
+          ],
+        },
+      ],
+      tabNote: [],
+    };
+  },
+  methods: {
+    playAudio() {
+      if (this.nbEcoute > 0 && this.play == true) {
+        this.play = false;
+        let audio = new Audio(this.test[0].audio);
+        audio.play();
+        this.nbEcoute--;
+        audio.addEventListener("ended", () => {
+          this.play = true;
+        });
+      }
+    },
+    check() {
+      this.indexLive++;
+    },
+  },
+  created: function shuffle() {
+    var i, j, tmp;
+    console.log(this.test[0].note[0]);
+    for (i = this.test[0].note.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      tmp = this.test[0].note[i];
+      this.test[0].note[i] = this.test[0].note[j];
+      this.test[0].note[j] = tmp;
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Crete Round", serif;
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+.gameBox {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  padding: 80px 0;
+}
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 80%;
+  padding: 80px 0;
+  font-size: 20px;
+}
+
+.left {
+  display: flex;
+  align-items: center;
+}
+.nbEcoute {
+  font-size: 30px;
+}
+.metronome {
+  display: flex;
+  width: 80%;
+  font-size: 30px;
+  justify-content: space-around;
+  align-items: center;
+}
+.playButton {
+  margin-left: 10px;
+  font-size: 30px;
+  cursor: pointer;
+  border-radius: 10px;
+  border: 2px solid black;
+  width: 80px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all ease 200ms;
+}
+.playButton:hover {
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.411);
+}
+
+.boxButton {
+  flex: 1;
+  width: 80%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
